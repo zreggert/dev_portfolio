9060e317e794429c1974505cf3d9738d7a7dcc49
elgalloloco <zreggert@gmail.com>
2021-09-07 17:01:09 -0400

    deleted some of the default files and added the background component

---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/styles.css';
+
+function Background() {
+    return (
+        <div>
+            <figure class="hero"></figure>
+            <figure class="mobile-hero"></figure>
+        </div>
+    )
+}
+
+export default Background;
