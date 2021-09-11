833623c5b5d91cfeb2b28ef8cb16a8dc87402d80
elgalloloco <zreggert@gmail.com>
2021-09-11 11:51:46 -0400

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,6 +9,10 @@ function AboutMe() {
             <section className="about-me" id="about-me">
                 <h2>About Me</h2>
                 <p>
+                    Full Stack Developer with a Web Development certificate from Georgia Tech University with
+                    skills in JavaScript and the MERN stack. Experienced service industry professional with strong communication
+                    skills and ability to work under pressure.
+
                     Hi! I'm Zack and I am a full stack developer from Atlanta, Georgia.
                     I began learning code after the 2020 corona virus pandemic left me without a job
                     for months and I became determined to learn a skill that would allow me to
